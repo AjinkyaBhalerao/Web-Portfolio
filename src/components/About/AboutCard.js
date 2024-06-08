780d8dd748f8918a61b0e07e797f1fab5386a7c0
ajinkya0919 <bhaleraoajinkya878@gmail.com>
2024-06-08 03:20:11 +0200

    Info Update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,19 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Ajinkya Bhalerao </span>
-            from <span className="purple"> Pune, India.</span>
+            Hi Everyone, I am <i><span className="purple">Ajinkya Bhalerao </span></i>
+            from <i><span className="purple"> Pune, India.</span></i>
             <br />
             I am currently working as a Data Science intern at Kheops.Ai in 
-            <span className="purple"> Nice, France </span>
+            <i><span className="purple"> Nice, France </span></i>
             <br />
-            I am pursiuing my <span className="purple">Mastère Spécialisé </span>
-            in Data Science at <span className="purple">Ecole Centrale de Marseille</span>
+            I am pursiuing my <i><span className="purple">Mastère Spécialisé </span></i>
+            in Data Science at <i><span className="purple">Ecole Centrale de Marseille</span></i>
             <br />
             This is also my final year of 
             <i>
               <span className="purple"> B.Tech in Computer Science and Engineering </span>
-            </i>at <span className="purple">Mahindra University, India</span>
+            </i>at <i><span className="purple">Mahindra University, India</span></i>
             <br />
             What I absolutely love to do out of my work desk:
           </p>
